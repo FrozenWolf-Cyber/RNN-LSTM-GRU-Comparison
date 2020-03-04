44ee31cb12c84d9da1d18609b39c6990089244d1
FrozenWolf-Cyber <57902078+FrozenWolf-Cyber@users.noreply.github.com>
2020-03-04 12:35:50 +0530

    Update trig_fnc_prdt.py

--- trig_fnc_prdt.py
+++ trig_fnc_prdt.py
@@ -1,40 +1,50 @@
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 import math
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
-x = [i / 10 for i in range(100)]
+x = [i for i in range(1000)]
+
+# CREATING RANDOM DATASET FOR EACH EPOCH
+# IT RETURNS 100 SIN(X) VALUES , NEXT 100 SIN(X) VALUES , INDEX OF THOSE 100 SIN(X) VALUES FOR PLOTTING
 
 
 def random_data_generator():
     y_temp = []
     x_temp = []
-    index_start = random.choice(range(10, 89))
-    for i in range(10):
+    index_start = random.choice(range(100, 890))
+    for i in range(100):
         x_temp.append(x[i + index_start])
+    x__ = x_temp.copy()
+    for i in range(100):
+        y_temp.append(((math.sin(x_temp[-1] + 0.1 * i)) * 30) + 5 * np.random.random(1))
 
-    y_temp.append(math.sin(x_temp[-1] + 0.1))
-
+    for i in range(100):
+        x_temp[i] = math.sin(x_temp[i])
     return (
-        torch.FloatTensor(x_temp).reshape((1, 1, 10)),
-        torch.FloatTensor(y_temp).reshape(1, 1, 1),
+        torch.FloatTensor(x_temp).reshape((1, 1, 100)),
+        torch.FloatTensor(y_temp).reshape(1, 1, 100),
+        x__,
     )
+
+
+# HERE THE MODEL HAS ONE LSTM LAYER AND ONE LINEAR LAYER
 
 
 class lstm_net(nn.Module):
     def __init__(self):
         super(lstm_net, self).__init__()
-        # self.layer1 = nn.Linear(10,10)
-        self.lstm_layer1 = nn.LSTM(10, 3)
-        self.layer_output = nn.Linear(3, 1)
-        self.hidden_initialzie = (torch.zeros((1, 1, 3)), torch.zeros((1, 1, 3)))
+        self.lstm_layer1 = nn.LSTM(100, 100)
+        self.layer_output = nn.Linear(100, 100)
+
+        # INITIALIZING OUR INITIAL HIDDEN VALUES (LONG TERM MEMORY VALUES)
+
+        self.hidden_initialize = (torch.zeros((1, 1, 100)), torch.zeros((1, 1, 100)))
 
     def forward(self, x):
-        # x = self.layer1(x)
-        x, _ = self.lstm_layer1(x, self.hidden_initialzie)
+        x, hidden = self.lstm_layer1(x, self.hidden_initialize)
         x = self.layer_output(x)
         return x
 
@@ -44,33 +54,32 @@ loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(my_model.parameters(), lr=0.001)
 loss_history = []
 
-for i in range(100):
+# TRAINNING LOOP
+
+for i in range(2000):
+    x_, y_, _ = random_data_generator()
+
     optimizer.zero_grad()
-    x_, y_ = random_data_generator()
     output = my_model(x_)
     loss = loss_function(output, y_)
     loss_history.append(loss)
     loss.backward()
     optimizer.step()
 
+# PRININTG FINAL LOSS
+
+print(loss_history[-1])
 plt.plot(loss_history)
 plt.show()
 
-# x_ , y_ = random_data_generator()
-#
-# print(x_.reshape(1,1,x_.shape[0])[0].shape)
-#
-# lstm = nn.LSTM(3, 3,2)  # Input dim is 3, output dim is 3
-# inputs = [torch.randn(1, 3) for _ in range(5)]  # make a sequence of length 5
-#
-# # initialize the hidden state.
-# hidden = (torch.randn(2, 1, 3),
-#           torch.randn(2, 1, 3))
-# for i in inputs:
-#     # Step through the sequence one element at a time.
-#     # after each step, hidden contains the hidden state.
-#     out, hidden = lstm(i.view(1, 1, -1), hidden)
-#
-# print(inputs[0].shape)
-# print(inputs[0].view(1, 1, -1).shape)
-# print(hidden[0])
+# PLOTTING OUT PREDICITON AND ACTUAL VALUES
+
+x_, y_, x_index = random_data_generator()
+model_prediction = my_model(x_)
+y = []
+for i in model_prediction[0][0]:
+    y.append(i)
+print(loss_function(model_prediction, y_))
+plt.plot(x_index, y)
+plt.plot(x_index, y_[0][0])
+plt.show()
